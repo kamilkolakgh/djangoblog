3095a6228300534297d15b816f3fb37964d6196c
studiocms <stud.cms@gmail.com>
2022-06-05 11:18:33 +0200

    change time

--- mysite/settings.py
+++ mysite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'pl-pl'
+LANGUAGE_CODE = 'utc'
 
 TIME_ZONE = 'Europe/Warsaw'
 
